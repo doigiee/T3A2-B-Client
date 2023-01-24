06e36fb33cfaa985cb100da172a29751f4f8be82
Ryan CHANG-HA LEE <13410@coderacademy.edu.au>
2023-01-24 10:23:56 +1000

    Added seen by cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,49 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import wave from '../assets/wave_green.svg'
+import urbanlist from '../assets/logos/Urban-List_Logo.png'
+import weekend from '../assets/logos/Weekend-Edition_Logo.png'
+import mustdo from '../assets/logos/Must-Do-Brisbane_Logo.png'
+
+const Card = (props) => {
+  
+  
+  return (
+  <>
+    <div className="seenby-cards">
+      <img src={props.src} width='170px'></img>
+      <span>{props.desc}</span>
+    </div>
+  </>
+  )
+}
+
+
+
 
 const Home = () => {
   return (
   <>
     <div className="heading-container">
       <h2 className="heading">Welcome to PAWFUL</h2>
-      <span></span>
       <p className="heading-description">
         Let us pamper your furry friend 
         at our professional dog grooming service!
       </p>
-      <div className="button-container">
+      <div className="btns-container">
         <Link className="btn">Make a booking</Link>
         <Link className="btn">Send inquiry</Link>
       </div>
     </div>
     <img id="wave" src={wave}></img>
     <section className="context-container">
+      <span>- Seen By -</span>
+      <div className="cards-container">
+        <Card src={urbanlist} desc='Number one!'/>
+        <Card src={mustdo} desc='Number one!'/>
+        <Card src={weekend} desc='Number one!'/>
+
+      </div>
     </section>
 
 
