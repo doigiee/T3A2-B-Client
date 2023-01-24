d20242b4170028ac3e6d5e03d70911f2d93f592c
Ryan CHANG-HA LEE <13410@coderacademy.edu.au>
2023-01-24 22:10:44 +1000

    Link tag fixed in Menu component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import { Routes, Route } from 'react-router-dom'
 import Home from './Home'
 import Footer from './Footer'
+import AboutUs from './AboutUs'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/aboutus' element={<AboutUs />} />
       </Routes>
     <Footer />
 
