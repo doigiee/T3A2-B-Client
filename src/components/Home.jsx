93a41ef7a229eb531ff30fe410af4cef75dadf94
Ryan CHANG-HA LEE <13410@coderacademy.edu.au>
2023-01-25 00:29:59 +1000

    CSS changed a bit for future components, About us page done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,33 +5,38 @@ import urbanlist from '../assets/logos/Urban-List_Logo.png'
 import weekend from '../assets/logos/Weekend-Edition_Logo.png'
 import mustdo from '../assets/logos/Must-Do-Brisbane_Logo.png'
 
-const Card = (props) => {
-  return (
-  <>
-    <div className="seenby-cards shadow-btm flex-col-cntr">
-      <img src={props.src} width='170px'></img>
-      <span>{props.desc}</span>
-    </div>
-  </>
-  )
-}
+
 
 const Home = () => {
+
+  const Card = (props) => {
+    return (
+    <>
+      <div className="seenby-cards shadow-btm flex-col-cntr">
+        <img src={props.src} width='170px'></img>
+        <span>{props.desc}</span>
+      </div>
+    </>
+    )
+  }
+
   return (
   <>
-    <div className="heading-container">
-      <h2 className="heading">Welcome to PAWFUL</h2>
-      <p className="heading-description">
-        Let us pamper your furry friend 
-        at our professional dog grooming service!
-      </p>
-      <div className="btns-container">
-        <Link className="btn shadow-btm">Make a booking</Link>
-        <Link className="btn shadow-btm">Send inquiry</Link>
+    <article id="home">
+      <div className="home-heading-container white">
+        <h2 className="heading">Welcome to PAWFUL</h2>
+        <p className="heading-description">
+          Let us pamper your furry friend 
+          at our professional dog grooming service!
+        </p>
+        <div className="btns-container">
+          <Link className="btn shadow-btm">Make a booking</Link>
+          <Link className="btn shadow-btm">Send inquiry</Link>
+        </div>
       </div>
-    </div>
-    <img id="wave" src={wave}></img>
-    <section className="context-container flex-col-cntr">
+    </article>
+    <img id="wave" src={wave}/>
+    <section className="context-container flex-col-cntr white bg-green">
       <span>- Seen By -</span>
       <div className="cards-container">
         <Card src={urbanlist} desc='Number one!'/>
