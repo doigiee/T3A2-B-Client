03b392c527fac9c2cda592b1677c9b592da9515b
Ryan CHANG-HA LEE <13410@coderacademy.edu.au>
2023-01-25 18:30:50 +1000

    Send inquiry invalid check built.

--- src/components/SendInquiry.jsx
+++ src/components/SendInquiry.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import wave from '../assets/wave_white.svg'
 import { Link } from 'react-router-dom'
 import emailjs from '@emailjs/browser';
@@ -8,11 +8,11 @@ import emailjs from '@emailjs/browser';
 const SendInquiry = () => {
 
   const form = useRef()
-
+  const [ isValidEmail, setValidEmail ] = useState(true)
+  const [ isValidPhone, setValidPhone ] = useState(true)
 
   const sendEmail = (e) => {
     e.preventDefault()
-
     emailjs.sendForm('service_ipqihi2', 'template_dp7mndu', form.current, 'x5ldj7hCbXs9GZBHH')
       .then((result) => {
           alert("Successfully sent!")
@@ -24,14 +24,17 @@ const SendInquiry = () => {
   const isEmail = (e) => {
     const patternEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
     const test = patternEmail.test(e.target.value)
-    if (!test) alert("Invalid email address")
+    if (!test) setValidEmail(false)
   }
 
   const isPhoneNumber = (n) => {
     const patternPhoneNumber = /\d{10}/
     const test = patternPhoneNumber.test(n.target.value)
-    if (!test || n.target.value.length !== 10 ) alert("Invalid phone number")
+    if (!test || n.target.value.length !== 10 ) setValidPhone(false)
   }
+
+  const toModifyEmail = () => setValidEmail(true)
+  const toModifyPhone = () => setValidPhone(true)
 
   return (
   <>
@@ -42,13 +45,13 @@ const SendInquiry = () => {
       <p className="heading-description">
         If you have any other inquiry, please don't hesitate to send us an email through the below form.
       </p>
-      <form className="send-inquiry-form flex column a-i-center" ref={form} onSubmit={sendEmail}>
+      <form className="send-inquiry-form flex column a-i-center" ref={form} onSubmit={toModifyEmail}>
         <input type="text" name="user_name" 
-              required placeholder="Name"/>
-        <input type="email" name="user_email" onBlur={isEmail}
-              required placeholder="Email"/>
+              required placeholder="Name *"/>
+        <input type="email" name="user_email" id={isValidEmail ? "" : "invalid-warning" } onBlur={isEmail} onFocus={toModifyEmail}
+              required placeholder="Email *"/>
         <input type="tel" name="user_phone"
-              placeholder="Phone" pattern="[0-9]{10}" onBlur={isPhoneNumber} 
+              placeholder="Phone *" pattern="[0-9]{10}" id={isValidPhone ? "" : "invalid-warning"} onBlur={isPhoneNumber} onFocus={toModifyPhone}
               required maxLength="10"/>
         <textarea name="message" id="inquiry-message" 
               placeholder="Message" cols="" rows="10"/>
